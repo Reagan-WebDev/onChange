a23bb219a7bd765ec7de1d0b9c71743b9323f1a5
Reagan <oluochreagano@gmail.com>
2025-11-19 16:48:31 +0300

    modified mycomponent

--- React-Practice1/src/assets/MyComponent.jsx
+++ React-Practice1/src/assets/MyComponent.jsx
@@ -4,15 +4,24 @@ import './MyComponent.css';
 function MyComponent() {
 
     const[name, setName]=useState("");
+    const[quantity, setQuantity]=useState()
 
     function handleNameChange(event){
         setName(event.target.value);
+    }
+
+    function handleQuantityChange(event){
+        setQuantity(event.target.value);
     }
 
     return(
         <div>
             <input type="text" value= {name} onChange = {handleNameChange} />
             <p className="myP">Name: {name}</p>
+
+            <input type="number" value= {quantity} onChange = {handleQuantityChange} />
+            <p className="myP">Quantity: {quantity}</p>
+
         </div>
     );
 }
